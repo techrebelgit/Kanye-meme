baf8c8958a41c9519a97cc6cb93adb7f549cc76a
albchamo <albchamo@gmail.com>
2025-02-02 00:35:06 -0800

    rank page

--- components/kanye-quote.tsx
+++ components/kanye-quote.tsx
@@ -5,14 +5,14 @@ import { useState, useEffect } from "react"
 const quotes = [
   "I am Warhol. I am the No. 1 most impactful artist of our generation. I am Shakespeare in the flesh.",
   "My greatest pain in life is that I will never be able to see myself perform live.",
-  "I'm a creative genius and there's no other way to word it.",
-  "I feel like I'm too busy writing history to read it.",
-  "I'm not a businessman, I'm a business, man.",
-  "Everything I'm not made me everything I am.",
-  "I think I do myself a disservice by comparing myself to Steve Jobs and Walt Disney and human beings that we've seen before. It should be more like Willy Wonka.",
+  "I&apos;m a creative genius and there&apos;s no other way to word it.",
+  "I feel like I&apos;m too busy writing history to read it.",
+  "I&apos;m not a businessman, I&apos;m a business, man.",
+  "Everything I&apos;m not made me everything I am.",
+  "I think I do myself a disservice by comparing myself to Steve Jobs and Walt Disney and human beings that we&apos;ve seen before. It should be more like Willy Wonka.",
   "I will go down as the voice of this generation, of this decade, I will be the loudest voice.",
-  "I'm on a pursuit of awesomeness. Excellence is the bare minimum.",
-  "I'm not always going to say things the perfect way, the right way. But I'm going to say how I feel.",
+  "I&apos;m on a pursuit of awesomeness. Excellence is the bare minimum.",
+  "I&apos;m not always going to say things the perfect way, the right way. But I&apos;m going to say how I feel.",
 ]
 
 export function KanyeQuote() {
@@ -22,6 +22,6 @@ export function KanyeQuote() {
     setQuote(quotes[Math.floor(Math.random() * quotes.length)])
   }, [])
 
-  return <div className="italic text-sm sm:text-base max-w-md" dangerouslySetInnerHTML={{ __html: `"${quote}"` }} />
+  return <div className="italic text-sm sm:text-base max-w-md">&quot;{quote}&quot;</div>
 }
 
