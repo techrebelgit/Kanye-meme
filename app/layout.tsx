235ddf6c7396582bbc62f9d576a695355f3b9ec5
albchamo <albchamo@gmail.com>
2025-02-01 16:28:54 -0800

    thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import "./globals.css"
 import type React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -8,34 +8,15 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "$KANYE Token",
   description: "The meme token that's stronger than ever.",
-  icons: {
-    icon: [
-      {
-        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/png-clipart-smiling-man-face-illustration-kanye-west-looking-music-stars-kanye-west-843sf57P8lb4tItgMr2NE2Ji1vKAg2.png",
-        type: "image/png",
-        sizes: "32x32",
-      },
-      {
-        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/png-clipart-smiling-man-face-illustration-kanye-west-looking-music-stars-kanye-west-843sf57P8lb4tItgMr2NE2Ji1vKAg2.png",
-        type: "image/png",
-        sizes: "16x16",
-      },
-    ],
-    apple: {
-      url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/png-clipart-smiling-man-face-illustration-kanye-west-looking-music-stars-kanye-west-843sf57P8lb4tItgMr2NE2Ji1vKAg2.png",
-      type: "image/png",
-      sizes: "180x180",
-    },
-  },
   openGraph: {
     title: "$KANYE Token",
     description: "The meme token that's stronger than ever.",
     images: [
       {
-        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/png-clipart-smiling-man-face-illustration-kanye-west-looking-music-stars-kanye-west-843sf57P8lb4tItgMr2NE2Ji1vKAg2.png",
-        width: 400,
-        height: 400,
-        alt: "$KANYE Token",
+        url: "/sitethumbnail.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Artistic portrait of Kanye in vibrant colors",
       },
     ],
   },
@@ -43,9 +24,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "$KANYE Token",
     description: "The meme token that's stronger than ever.",
-    images: [
-      "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/png-clipart-smiling-man-face-illustration-kanye-west-looking-music-stars-kanye-west-843sf57P8lb4tItgMr2NE2Ji1vKAg2.png",
-    ],
+    images: ["/sitethumbnail.jpg"],
   },
 }
 
@@ -56,14 +35,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="shortcut icon"
-          href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/png-clipart-smiling-man-face-illustration-kanye-west-looking-music-stars-kanye-west-843sf57P8lb4tItgMr2NE2Ji1vKAg2.png"
-        />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
+
 
