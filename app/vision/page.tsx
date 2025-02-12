2cdc5a6df40763a1ad14e8d94f412ae734bce8ed
albchamo <albchamo@gmail.com>
2025-02-11 18:09:55 -0800

    Remove Header from Ranchcheck

--- app/vision/page.tsx
+++ app/vision/page.tsx
@@ -1,11 +1,10 @@
-import Header from "../components/Header"
+
 import Footer from "../components/Footer"
 import VisionContent from "./vision-content"
 
 export default function VisionPage() {
   return (
     <div className="min-h-screen bg-[#F5F5F5] text-[#333333] flex flex-col">
-      <Header />
       <main className="flex-grow">
         <VisionContent />
       </main>
