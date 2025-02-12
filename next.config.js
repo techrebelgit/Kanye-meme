6ad84bd66fe5c69d3394631404294d2f5d711327
albchamo <albchamo@gmail.com>
2025-02-11 18:23:49 -0800

    Remove Header from Ranchcheck

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    eslint: {
-      dirs: ["pages", "components", "lib", "layouts", "utils", "app"],
-    },
-  }
-  
-  module.exports = nextConfig
-  
-  
+  reactStrictMode: true,
+  eslint: {
+    dirs: ["pages", "components", "lib", "layouts", "utils", "app"],
+  },
+  images: {
+    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+  },
+}
+
+module.exports = nextConfig
+
